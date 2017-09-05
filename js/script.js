a338d3d08814588e0c04db3a593eb38de44b4e9f
SkowronekArt <anna@skowronek.art.pl>
2017-09-05 14:26:27 +0200

    Probable conflict

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ var countriesList = $('#countries');
 //			countries
 $('#search-country').click(searchCountries);
 
-
-
-
 function searchCountries() {
  	var countryName = $('#country-name').val();
 	if(!countryName.length) countryName = 'Poland';
@@ -18,95 +15,15 @@ function searchCountries() {
 	});
 }
 
-
 function showCountriesList(resp) {
+	console.log(resp);
   	countriesList.empty();
 	resp.forEach(function(item){
-	//Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
-	   	$('<li>').text(item.name).appendTo(countriesList);
+	   	var img = $('<img>').attr('src', item.flag).css('width', '25px');
+	   	var capital = $('<span>').text(item.capital);
+	   	$('<li>').text(item.name)
+	   			.appendTo(countriesList)
+	   			.append(img)
+	   			.append($('<span>').text(item.capital));
 	});
 }
-
-
-/*
-
-
-var urlCountry = 'https://restcountries.eu/rest/v2/name/';
-var countriesList = $('#countries');
-
-var urlFlag = 'https://restcountries.eu/data/';
-var flagList = $('#flags');
-
-console.log(flagList)
- 
-
-//			countries
-
-
-$(function() {
-    $('#search-country').click(Country.printInfo());
-});
-
-
-
-
-function Country(country, flag) {
-	this.country = function searchCountries() {
-	 	var country = $('#country-name').val();
-		if(!countryName.length) countryName = 'Poland';
-		$.ajax({
-		  	url: urlCountry + countryName,
-			method: 'GET',
-		  	success: showCountriesList
-	  	});
-	}
-	this.flag = function searchFlag() {
-		$.ajax({
-			url: urlFlag + this.country + '.svg',
-			method: 'GET',
-			successive: alert("yes")
-			});
-	}
-}
-
-
-Country.prototype.printInfo = function() {
-	var print = `${this.country} ma taką flagę ${this.flag}$.`;
-};
-
-
-
-function searchCountries() {
- 	var countryName = $('#country-name').val();
-	if(!countryName.length) countryName = 'Poland';
-	$.ajax({
-	  	url: urlCountry + countryName,
-		method: 'GET',
-	  	success: showCountriesList
-  	});
-}
-
-
-function showCountriesList(resp) {
-  	countriesList.empty();
-	resp.forEach(function(countryItem) {
-   		//Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
-   		$('<li>').text(countryItem.name).appendTo(countriesList);
-	});
-}
-
- 
-
-function showFlag(respFlag) {
-  	flagList.empty();
-	respFlag.forEach(function(flagItem) {
-   		//Here is the code that will execute on each successive item in the collection. A single item is hidden under an item variable.
-   		$('<li>').text(flagItem.text).appendTo(flagList);
-
-	});
-}
-
-
-
-
-*/
